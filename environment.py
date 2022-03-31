fa016b0c943536ac1547acd25a4d31833892221f
matthew-j <matthew.mej@utexas.edu>
2022-03-31 14:42:16 -0500

    Added environment and model files

---
+++ environment.py
@@ -0,0 +1,63 @@
+import gym
+from gym.spaces import Box
+from gym.wrappers import FrameStack
+import np
+import torch
+from torchvision import transforms
+import gym_super_mario_bros
+from nes_py.wrappers import JoypadSpace
+
+## Create wrappers for environment. Inspired by: https://pytorch.org/tutorials/intermediate/mario_rl_tutorial.html
+class SkipFrames(gym.wrapper):
+    def __init__(self, env, skip):
+        super().__init__(env)
+        self.num_skips = skip
+    
+    def step(self, action):
+        tot_reward = 0
+        done = False
+        for _ in range(self.num_skips):
+            state, reward, info = self.env.step(action)
+            tot_reward += reward
+            if done:
+                break
+        return state, tot_reward, done, info 
+
+class GrayScale(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        obs_shape = self.observation_space.shape[:2]
+        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
+
+    def observation(self, observation):
+        observation = np.transpose(observation, (2, 0, 1))
+        observation = torch.tensor(observation.copy(), dtype=torch.float)
+        observation = transforms.Grayscale(observation)
+        return observation
+
+class ResizeObservation(gym.ObservationWrapper):
+    def __init__(self, env, shape):
+        super().__init__(env)
+        if isinstance(shape, int):
+            self.shape = (shape, shape)
+        else:
+            self.shape = tuple(shape)
+
+        obs_shape = self.shape + self.observation_space.shape[2:]
+        self.observation_space = Box(low=0, high=255, shape=obs_shape, dtype=np.uint8)
+
+    def observation(self, observation):
+        observation = transforms.Resize(self.shape)
+        observation = transforms.Normalize(0, 255).squeeze(0)
+        return observation
+
+def generate_env():
+    env = gym_super_mario_bros.make("SuperMarioBros-1-1-v0")
+    env = JoypadSpace(env, [["right"], ["right", "A"]])
+    env.reset()
+    env = SkipFrames(env, skip=4)
+    env = GrayScale(env)
+    env = ResizeObservation(env, shape=84)
+    env = FrameStack(env, num_stack=4)
+
+    return env
---
+++ models.py
@@ -0,0 +1,25 @@
+from torch import nn
+
+class MiniCnn(nn.Module):
+    """mini cnn structure
+  input -> (conv2d + relu) x 3 -> flatten -> (dense + relu) x 2 -> output
+  """
+
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        c, h, w = input_dim
+
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=c, out_channels=32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Flatten(0),
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, output_dim),
+        )
+    def forward(self, input):
+        return self.model(input)
